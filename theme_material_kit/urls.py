7187b21f8e1d47a4dcc673e2407d0bfd2c1ff9be
mominur-helios <mominur.rahman@heliosinsider.com>
2022-11-20 12:53:58 +0600

    addeadded theme and authentication

---
+++ theme_material_kit/urls.py
@@ -0,0 +1,30 @@
+from django.urls import path
+from theme_material_kit import views
+from django.contrib.auth import views as auth_views
+
+
+urlpatterns = [
+    # Pages
+    path('', views.index, name="index"),
+    path('contact-us/', views.contact_us, name='contact-us'),
+    path('about-us/', views.about_us, name='about-us'),
+    path('author/', views.author, name='author'),
+
+    # Authentication
+    path('accounts/login/', views.UserLoginView.as_view(), name='login'),
+    path('accounts/logout/', views.user_logout_view, name='logout'),
+    path('accounts/register/', views.registration, name='register'),
+    path('accounts/password-change/', views.UserPasswordChangeView.as_view(), name='password_change'),
+    path('accounts/password-change-done/', auth_views.PasswordChangeDoneView.as_view(
+        template_name='accounts/password_change_done.html'
+    ), name="password_change_done" ),
+    path('accounts/password-reset/', views.UserPasswordResetView.as_view(), name='password_reset'),
+    path('accounts/password-reset-confirm/<uidb64>/<token>/', 
+        views.UserPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('accounts/password-reset-done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='accounts/password_reset_done.html'
+    ), name='password_reset_done'),
+    path('accounts/password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='accounts/password_reset_complete.html'
+    ), name='password_reset_complete'),
+]
